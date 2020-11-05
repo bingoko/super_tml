698fa2b6013427a06c2207d2e49082df3a216f2b
Bingoko <bob.li.0718@gmail.com>
2020-11-05 20:28:40 +0000

    enable label

--- src/opt/training_pipeline.py
+++ src/opt/training_pipeline.py
@@ -56,7 +56,7 @@ def opt_selection(model, opt=args.opt):
 
 
 def train_model(model_name='densenet121', opt='Adagrad', dataset='iris', writer=None, label_col_name=''):
-    train_loader, val_loader, test_loader = load_data(dataset, label_col_name)
+    train_loader, val_loader, test_loader = load_data(dataset, label_col_name=label_col_name)
 
     # Model selection
     model = load_model(model_name)
