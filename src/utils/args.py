46f1e7228047e52e281ae1b293764d693116f39e
Bingoko <bob.li.0718@gmail.com>
2020-11-05 19:55:17 +0000

    enable label

--- src/utils/args.py
+++ src/utils/args.py
@@ -18,6 +18,8 @@ def parser():
 
     PARSER.add_argument('--dataset', default='iris', type=str,
                         help='Dataset.')
+    PARSER.add_argument('--label', default='', type=str,
+                        help='Label.')
     PARSER.add_argument('--model', default='densenet121', type=str,
                         choices=['resnet18', 'densenet121'], help='Model.')
     PARSER.add_argument('--opt', default='Adagrad', type=str,
